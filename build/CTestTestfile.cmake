aa3c29ff565bcfc87c48cd4a3d47abbb9e3e0bc2
SyrupOnWaffles <nicholaskor@hotmail.com>
2021-11-12 11:39:59 -0700

    x

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/syrup/Desktop/Programming/C++/Uke-Project
+# Build directory: /home/syrup/Desktop/Programming/C++/Uke-Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
