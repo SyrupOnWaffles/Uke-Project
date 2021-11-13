542956581c251fccd02e03431c145b1a1b725373
SyrupOnWaffles <nicholaskor@hotmail.com>
2021-11-12 21:02:07 -0700

    build script for linux

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cmake ../
 make
 #? Remove Unnecesary files
 rm CMakeFiles/ -r; rm Testing/ -r; rm cmake_install.cmake; rm CMakeCache.txt; rm CTestTestfile.cmake; rm DartConfiguration.tcl; rm main.cpp; rm Makefile
-#? Run
+#? Run 
 ./ukulele
