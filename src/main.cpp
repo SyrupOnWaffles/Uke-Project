2583f81d0bb84bca91c53e169c942c66e1cfa224
SyrupOnWaffles <nicholaskor@hotmail.com>
2021-11-13 18:37:55 -0700

    making strumming with mouse smoother (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,7 @@ int main()
    test_init(font, "font");
 
    ALLEGRO_SAMPLE *noteSamples[4] = {
-       al_load_sample("sounds/A.wav"), al_load_sample("sounds/E.wav"), al_load_sample("sounds/C.wav"), al_load_sample("sounds/G.wav")
-};
+       al_load_sample("sounds/A.wav"), al_load_sample("sounds/E.wav"), al_load_sample("sounds/C.wav"), al_load_sample("sounds/G.wav")};
 
    ALLEGRO_BITMAP *uke = al_load_bitmap("images/uke.png");
    test_init(uke, "uke");
@@ -51,12 +50,12 @@ int main()
    al_register_event_source(queue, al_get_display_event_source(disp));
    al_register_event_source(queue, al_get_timer_event_source(timer));
    al_register_event_source(queue, al_get_mouse_event_source());
-   
+
    bool done = false;
    bool redraw = true;
    ALLEGRO_EVENT event;
-   #define KEY_SEEN     1
-   #define KEY_RELEASED 2
+#define KEY_SEEN 1
+#define KEY_RELEASED 2
 
    unsigned char key[ALLEGRO_KEY_MAX];
    memset(key, 0, sizeof(key));
@@ -64,8 +63,8 @@ int main()
    int two;
    int three;
    int four;
-   int x;
-   int y;
+   int x = 1280;
+   int y = 720;
    bool fourPlaying;
    bool threePlaying;
    bool twoPlaying;
@@ -73,121 +72,145 @@ int main()
    al_hide_mouse_cursor(disp);
    al_start_timer(timer);
    while (1)
-   {  
-      
+   {
+
       float mean = ((one + two + three + four) / 4) - 1;
-      fourPlaying = false;
-      threePlaying = false;
-      twoPlaying = false;
-      onePlaying = false;   
       al_wait_for_event(queue, &event);
-      
-      switch(event.type)
-        {
-            
-            case ALLEGRO_EVENT_TIMER:
-               one = 0;
-               two = 0;
-               three = 0;
-               four = 0;
-               //yandere dev be like
-               if(key[ALLEGRO_KEY_Z])
-                  four = 1;
-               if(key[ALLEGRO_KEY_X])
-                  four = 2;
-               if(key[ALLEGRO_KEY_C])
-                  four = 3;
-               if(key[ALLEGRO_KEY_V])
-                  four = 4;
-               if(key[ALLEGRO_KEY_B])
-                  four = 5;
-               if(key[ALLEGRO_KEY_N])
-                  four = 6;
-               if(key[ALLEGRO_KEY_M])
-                  four = 7;
-               if(key[ALLEGRO_KEY_A])
-                  three = 1;
-               if(key[ALLEGRO_KEY_S])
-                  three = 2;
-               if(key[ALLEGRO_KEY_D])
-                  three = 3;
-               if(key[ALLEGRO_KEY_F])
-                  three = 4;
-               if(key[ALLEGRO_KEY_G])
-                  three = 5;
-               if(key[ALLEGRO_KEY_H])
-                  three = 6;
-               if(key[ALLEGRO_KEY_J])
-                  three = 7;
-               if(key[ALLEGRO_KEY_Q])
-                  two = 1;
-               if(key[ALLEGRO_KEY_W])
-                  two = 2;
-               if(key[ALLEGRO_KEY_E])
-                  two = 3;
-               if(key[ALLEGRO_KEY_R])
-                  two = 4;
-               if(key[ALLEGRO_KEY_T])
-                  two = 5;
-               if(key[ALLEGRO_KEY_Y])
-                  two = 6;
-               if(key[ALLEGRO_KEY_U])
-                  two = 7;
-               if(key[ALLEGRO_KEY_1])
-                  one = 1;
-               if(key[ALLEGRO_KEY_2])
-                  one = 2;
-               if(key[ALLEGRO_KEY_3])
-                  one = 3;
-               if(key[ALLEGRO_KEY_4])
-                  one = 4;
-               if(key[ALLEGRO_KEY_5])
-                  one = 5;
-               if(key[ALLEGRO_KEY_6])
-                  one = 6;
-               if(key[ALLEGRO_KEY_7])
-                  one = 7;
-               if(key[ALLEGRO_KEY_ESCAPE ])
-                  done = true;
-                for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
-                    key[i] &= KEY_SEEN;
 
-                redraw = true;
-                break;
+      switch (event.type)
+      {
 
-            case ALLEGRO_EVENT_KEY_DOWN:
-                  key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
-               
-               	break;
-            case ALLEGRO_EVENT_KEY_UP:
-                key[event.keyboard.keycode] &= KEY_RELEASED;
-                break;
-            case ALLEGRO_EVENT_MOUSE_AXES:
-               x = event.mouse.x;
-               y = event.mouse.y;
-               if(y == 283 && x > 300 && x < 1000) onePlaying = true;   
-               if(y == 303 && x > 300 && x < 1000) twoPlaying = true; 
-               if(y == 323 && x > 300 && x < 1000) threePlaying = true;
-               if(y == 343 && x > 300 && x < 1000)fourPlaying = true;
-                       
-                  
-               break;
-            case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                done = true;
-                break;
-           
-        }
-      if(fourPlaying) al_play_sample(noteSamples[3], 1.0, 0.0, 1.0 + (0.05946 * four), ALLEGRO_PLAYMODE_ONCE, NULL);
-      if(threePlaying) al_play_sample(noteSamples[2], 1.0, 0.0, 1.0 + (0.05946 * three), ALLEGRO_PLAYMODE_ONCE, NULL);      
-      if(twoPlaying) al_play_sample(noteSamples[1], 1.0, 0.0, 1.0 + (0.05946 * two), ALLEGRO_PLAYMODE_ONCE, NULL);      
-      if(onePlaying) al_play_sample(noteSamples[0], 1.0, 0.0, 1.0 + (0.05946 * one), ALLEGRO_PLAYMODE_ONCE, NULL);
+      case ALLEGRO_EVENT_TIMER:
+         one = 0;
+         two = 0;
+         three = 0;
+         four = 0;
+         // yandere dev be like
+         if (key[ALLEGRO_KEY_Z])
+            four = 1;
+         if (key[ALLEGRO_KEY_X])
+            four = 2;
+         if (key[ALLEGRO_KEY_C])
+            four = 3;
+         if (key[ALLEGRO_KEY_V])
+            four = 4;
+         if (key[ALLEGRO_KEY_B])
+            four = 5;
+         if (key[ALLEGRO_KEY_N])
+            four = 6;
+         if (key[ALLEGRO_KEY_M])
+            four = 7;
+         if (key[ALLEGRO_KEY_A])
+            three = 1;
+         if (key[ALLEGRO_KEY_S])
+            three = 2;
+         if (key[ALLEGRO_KEY_D])
+            three = 3;
+         if (key[ALLEGRO_KEY_F])
+            three = 4;
+         if (key[ALLEGRO_KEY_G])
+            three = 5;
+         if (key[ALLEGRO_KEY_H])
+            three = 6;
+         if (key[ALLEGRO_KEY_J])
+            three = 7;
+         if (key[ALLEGRO_KEY_Q])
+            two = 1;
+         if (key[ALLEGRO_KEY_W])
+            two = 2;
+         if (key[ALLEGRO_KEY_E])
+            two = 3;
+         if (key[ALLEGRO_KEY_R])
+            two = 4;
+         if (key[ALLEGRO_KEY_T])
+            two = 5;
+         if (key[ALLEGRO_KEY_Y])
+            two = 6;
+         if (key[ALLEGRO_KEY_U])
+            two = 7;
+         if (key[ALLEGRO_KEY_1])
+            one = 1;
+         if (key[ALLEGRO_KEY_2])
+            one = 2;
+         if (key[ALLEGRO_KEY_3])
+            one = 3;
+         if (key[ALLEGRO_KEY_4])
+            one = 4;
+         if (key[ALLEGRO_KEY_5])
+            one = 5;
+         if (key[ALLEGRO_KEY_6])
+            one = 6;
+         if (key[ALLEGRO_KEY_7])
+            one = 7;
+         if (key[ALLEGRO_KEY_ESCAPE])
+            done = true;
+         for (int i = 0; i < ALLEGRO_KEY_MAX; i++)
+            key[i] &= KEY_SEEN;
+
+         redraw = true;
+         break;
+
+      case ALLEGRO_EVENT_KEY_DOWN:
+         key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
+
+         break;
+      case ALLEGRO_EVENT_KEY_UP:
+         key[event.keyboard.keycode] &= KEY_RELEASED;
+         break;
+      case ALLEGRO_EVENT_MOUSE_AXES:
+         x = event.mouse.x;
+         y = event.mouse.y;
+         // terrible collision (is this even collision detection?)
+         if (y == 283 && x > 300 && x < 1000)
+         {
+            onePlaying = true;
+         }
+         if (y == 303 && x > 300 && x < 1000)
+         {
+            twoPlaying = true;
+         }
+         if (y == 323 && x > 300 && x < 1000)
+         {
+            threePlaying = true;
+         }
+         if (y == 343 && x > 300 && x < 1000)
+         {
+            fourPlaying = true;
+         }
+
+         break;
+      case ALLEGRO_EVENT_DISPLAY_CLOSE:
+         done = true;
+         break;
+      }
+      if (fourPlaying)
+      {
+         al_play_sample(noteSamples[3], 1.0, 0.0, 1.0 + (0.05946 * four), ALLEGRO_PLAYMODE_ONCE, NULL);
+         fourPlaying = false;
+      }
+      if (threePlaying)
+      {
+         al_play_sample(noteSamples[2], 1.0, 0.0, 1.0 + (0.05946 * three), ALLEGRO_PLAYMODE_ONCE, NULL);
+         threePlaying = false;
+      }
+      if (twoPlaying)
+      {
+         al_play_sample(noteSamples[1], 1.0, 0.0, 1.0 + (0.05946 * two), ALLEGRO_PLAYMODE_ONCE, NULL);
+         twoPlaying = false;
+      }
+      if (onePlaying)
+      {
+         al_play_sample(noteSamples[0], 1.0, 0.0, 1.0 + (0.05946 * one), ALLEGRO_PLAYMODE_ONCE, NULL);
+         onePlaying = false;
+      }
+
       if (done)
          break;
 
       if (redraw && al_is_event_queue_empty(queue))
       {
          al_clear_to_color(al_map_rgb(99, 155, 255));
-         
+
          al_draw_scaled_bitmap(uke, 0, 0, 900, 750, 10, 100, 1200, 1000, 1);
 
          // Draw Strings
@@ -195,28 +218,25 @@ int main()
          al_draw_ellipse(650, 303, 355, .1, al_map_rgb(69, 40, 60), 3);
          al_draw_ellipse(650, 323, 355, .1, al_map_rgb(69, 40, 60), 3);
          al_draw_ellipse(650, 343, 355, .1, al_map_rgb(69, 40, 60), 3);
-         
 
-         //Fret Markers
+         // Fret Markers
          al_draw_filled_circle(275 + one * 40, 283, 10, al_map_rgb(172, 50, 50));
          al_draw_filled_circle(275 + two * 40, 303, 10, al_map_rgb(172, 50, 50));
          al_draw_filled_circle(275 + three * 40, 323, 10, al_map_rgb(172, 50, 50));
          al_draw_filled_circle(275 + four * 40, 343, 10, al_map_rgb(172, 50, 50));
 
-         al_draw_bitmap(hand, x, y,0);
+         al_draw_bitmap(hand, x, y, 0);
 
          std::stringstream ss;
          ss << four << three << two << one;
          std::string strNum = ss.str();
-         const char * c = strNum.c_str();
+         const char *c = strNum.c_str();
          al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 0, 0, c);
-
 
          al_flip_display();
 
          redraw = false;
       }
-      
    }
 
    // Destruction
